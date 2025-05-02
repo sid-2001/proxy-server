e625ccd84b040efaf18a48130419d3f4fe375c9a
Siddhant kaushik <64539683+sid-2001@users.noreply.github.com>
2025-05-02 19:20:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -430,3 +430,18 @@ console.log("origanl url",backendURL)
 app.listen(PORT, () => {
   console.log(`🚀 Proxy server running on http://localhost:${5000}`);
 });
+const url = `https://proxy-server-n7ib.onrender.com`; // Replace with your Render URL
+const interval = 30000; // Interval in milliseconds (30 seconds)
+
+//Reloader Function
+function reloadWebsite() {
+  axios.get(url)
+    .then(response => {
+      console.log(`Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`);
+    })
+    .catch(error => {
+      console.error(`Error reloading at ${new Date().toISOString()}:`, error.message);
+    });
+}
+
+setInterval(reloadWebsite, interval);
